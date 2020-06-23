56a9c94d504aa920ee84f26c592ec54ed66e6cd1
王玉林 <luther_yulin@163.com>
2020-06-23 22:25:21 +0800

    添加了test方法

--- git-test/src/cn/yulin/GitTest.java
+++ git-test/src/cn/yulin/GitTest.java
@@ -4,4 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git使用测试");
     }
+
+    public void test(){
+        System.out.println("添加test方法");
+    }
 }
